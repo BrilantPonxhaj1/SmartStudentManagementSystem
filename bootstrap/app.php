55329b600fd7a8c6275f47aa06f598a1ad415876
Afrim Ymeri <78933251+afrimymeri@users.noreply.github.com>
2025-03-30 00:10:08 +0100

    Configured CORS
    Added the cors.php file with ACAOs that allows the communication between backend and frontend

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Middleware\HandleCors;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(append: [
+            HandleCors::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
     })
---
+++ config/cors.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['GET, POST, PUT, DELETE, OPTIONS'],
+    'allowed_origins' => ['*'], // Change '*' to specific domains for security
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
